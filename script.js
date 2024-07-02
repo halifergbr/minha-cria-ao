b7969758ea306a7d12c9380d69c01d8e7abc9542
halifergbr <127866807+halifergbr@users.noreply.github.com>
2024-07-02 09:01:18 -0300

    d
    f

---
+++ script.js
@@ -0,0 +1,10 @@
+<script src="vanilla-tilt.js"></script>
+<script type="text/javascript">
+    vanillatilt.init(document.querySelectorAll(".card"),{
+        max:25,
+        speed:5000,
+        glare:true,
+        'max-glare':0.5,
+        });
+    
+    </script>
